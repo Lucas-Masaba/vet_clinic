147a733a801f817fb5f5c97a578f7eeef99b4969
Lucas-Masaba <masabaluke@gmail.com>
2022-02-18 03:10:47 +0300

    join tables

--- data.sql
+++ data.sql
@@ -57,3 +57,42 @@ UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
 UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
 UPDATE animals SET owner_id = 4 WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
 UPDATE animals SET owner_id = 5 WHERE name = 'Angemon' OR name = 'Boarmon';
+
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('William Thatcher', 45, '2000-04-23');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Maisy Smith', 26, '2019-01-17');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Stephanie Mendez', 64, '1981-05-04');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO specializations (vets_name, species_id, vets_id) VALUES ('William Thatcher', 1, 1);
+INSERT INTO specializations (vets_name, species_id, vets_id) VALUES ('Stephanie Mendez', 2, 3);
+INSERT INTO specializations (vets_name, species_id, vets_id) VALUES ('Stephanie Mendez', 1, 3);
+INSERT INTO specializations (vets_name, species_id, vets_id) VALUES ('Jack Harkness', 2, 4);
+INSERT INTO specializations (vets_name, species_id, vets_id) VALUES ('Maisy Smith', NULL, 2);
+
+INSERT INTO visits (animals_name, date_of_visit, animals_id, vets_id) VALUES ('Agumon', '2020-05-24', 1, 1);
+INSERT INTO visits (animals_name, date_of_visit, animals_id, vets_id) VALUES ('Agumon', '2020-07-22', 1, 3);
+INSERT INTO visits (animals_name, date_of_visit, animals_id, vets_id) VALUES ('Gabumon', '2021-02-02', 2, 4);
+INSERT INTO visits (animals_name, date_of_visit, animals_id, vets_id) VALUES ('Pickachu', '2020-01-05', 3, 2);
+INSERT INTO visits (animals_name, date_of_visit, animals_id, vets_id) VALUES ('Pickachu', '2020-03-08', 3, 2);
+INSERT INTO visits (animals_name, date_of_visit, animals_id, vets_id) VALUES ('Devimon', '2021-05-04', 4, 3);
+INSERT INTO visits (animals_name, date_of_visit, animals_id, vets_id) VALUES ('Charmander', '2021-02-24', 5, 4);
+INSERT INTO visits (animals_name, date_of_visit, animals_id, vets_id) VALUES ('Squirtle', '2019-09-29', 7, 3);
+INSERT INTO visits (animals_name, date_of_visit, animals_id, vets_id) VALUES ('Angemon', '2020-10-03', 8, 4);
+INSERT INTO visits (animals_name, date_of_visit, animals_id, vets_id) VALUES ('Angemon', '2020-11-04', 8, 4);
+INSERT INTO visits (animals_name, date_of_visit, animals_id, vets_id) VALUES ('Boarmon', '2019-01-24', 9, 2);
+INSERT INTO visits (animals_name, date_of_visit, animals_id, vets_id) VALUES ('Boarmon', '2019-05-15', 9, 2);
+INSERT INTO visits (animals_name, date_of_visit, animals_id, vets_id) VALUES ('Boarmon', '2020-02-27', 9, 2);
+INSERT INTO visits (animals_name, date_of_visit, animals_id, vets_id) VALUES ('Boarmon', '2020-08-03', 9, 2);
+INSERT INTO visits (animals_name, date_of_visit, animals_id, vets_id) VALUES ('Blossom', '2020-05-24', 10, 3);
+INSERT INTO visits (animals_name, date_of_visit, animals_id, vets_id) VALUES ('Blossom', '2020-01-11', 10, 1);
+
+
+
+
+
+
+
+
+
+
+
--- schema.sql
+++ schema.sql
@@ -22,8 +22,40 @@ CREATE TABLE species (
 
 ALTER TABLE animals ADD species TEXT;
 
+ALTER TABLE animals DROP species;
+
 ALTER TABLE animals ADD species_id INT;
 ALTER TABLE animals ADD CONSTRAINT speicies_id_fk FOREIGN KEY (species_id) REFERENCES species(id);
 
 ALTER TABLE animals ADD owner_id INT;
-ALTER TABLE animals ADD CONSTRAINT owner_fk FOREIGN KEY (owner_id) REFERENCES owners;
+ALTER TABLE animals ADD CONSTRAINT owner_fk FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+   id                 INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+   name               VARCHAR(50)                      NOT NULL,
+   age                INT                              NOT NULL,
+   date_of_graduation date                             NOT NULL
+);
+
+CREATE TABLE specializations (
+   vets_name          VARCHAR(50)                      NOT NULL,
+   species_id         INT                              NOT NULL,
+   vets_id            INT                              NOT NULL
+);
+
+ALTER TABLE specializations ALTER COLUMN species_id DROP NOT NULL;
+
+ALTER TABLE specializations ADD CONSTRAINT specializations_fk FOREIGN KEY (species_id) REFERENCES  species(id);
+ALTER TABLE specializations ADD CONSTRAINT specializations_vets_fk FOREIGN KEY (vets_id) REFERENCES  vets(id);
+
+CREATE TABLE visits (
+   animals_name  VARCHAR(50)                      NOT NULL,
+   date_of_visit date                             NOT NULL,
+   animals_id    INT                              NOT NULL,
+   vets_id       INT                              NOT NULL
+);
+
+ALTER TABLE visits ADD CONSTRAINT visits_fk FOREIGN KEY (animals_id) REFERENCES  animals(id);
+ALTER TABLE visits ADD CONSTRAINT visits_vets_fk FOREIGN KEY (vets_id) REFERENCES  vets(id);
+
+ALTER TABLE specializations ALTER COLUMN species_id DROP NOT NULL;
